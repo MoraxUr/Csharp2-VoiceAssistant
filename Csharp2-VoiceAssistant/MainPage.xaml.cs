f2122fa0d81a3e1c43023be5b48ac73b6aefb701
MoraxUr <91067492+MoraxUr@users.noreply.github.com>
2024-08-12 16:57:06 +0200

    Mainpage buttons added

--- Csharp2-VoiceAssistant/MainPage.xaml.cs
+++ Csharp2-VoiceAssistant/MainPage.xaml.cs
@@ -6,4 +6,19 @@ public MainPage()
     {
         InitializeComponent();
     }
+
+    private async void Instructions_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new Instructions());
+    }
+
+    private async void History_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new History());
+    }
+
+    private async void Settings_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new Settings());
+    }
 }
