7bb4a9d48b12af6d3fe3fb4149d590db9f954a4d
MoraxUr <91067492+MoraxUr@users.noreply.github.com>
2024-08-16 13:53:40 +0200

    Create CSVHelperService.cs

---
+++ Csharp2-VoiceAssistant/CSVHelperService.cs
@@ -0,0 +1,41 @@
+﻿namespace Csharp2_VoiceAssistant
+{
+    public class CSVHelperService
+    {
+        public CSVHelperService()
+        {
+            string csvPath = GetCSVPath();
+            
+        }
+
+        public static string GetProjectRootDirectory()
+        {
+            DirectoryInfo? currentDirectory = new(AppContext.BaseDirectory);
+
+            while (currentDirectory != null && !currentDirectory.GetFiles("*.csproj").Any())
+            {
+                currentDirectory = currentDirectory.Parent;
+            }
+
+            if (currentDirectory == null)
+            {
+                throw new DirectoryNotFoundException("Project root directory not found.");
+            }
+
+            return currentDirectory.FullName;
+        }
+
+        public static string GetCSVPath()
+        {
+            var projectRoot = GetProjectRootDirectory();
+            var csvPath = Path.Combine(projectRoot, "Instructions", "Instructions.csv");
+
+            if (!Directory.Exists(csvPath))
+            {
+                // Make CSV if not present
+            }
+
+            return csvPath;
+        }
+    }
+}
